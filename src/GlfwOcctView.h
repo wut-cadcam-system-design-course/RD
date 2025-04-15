c1b41d9690cb53c502d5fcfeffd69ef2dae229dc
Sebastian Szarafin <sszarafin@merlin.pho>
2025-04-15 13:17:37 +0200

    refactor: clang format

--- src/GlfwOcctView.h
+++ src/GlfwOcctView.h
@@ -31,20 +31,19 @@
 //! Sample class creating 3D Viewer within GLFW window.
 class GlfwOcctView : protected AIS_ViewController
 {
-public:
+ public:
   //! Default constructor.
-	GlfwOcctView();
+  GlfwOcctView();
 
   //! Destructor.
-	~GlfwOcctView();
+  ~GlfwOcctView();
 
   //! Main application entry point.
   void run();
 
-private:
-
+ private:
   //! Create GLFW window.
-  void initWindow (int theWidth, int theHeight, const char* theTitle);
+  void initWindow(int theWidth, int theHeight, const char* theTitle);
 
   //! Create 3D Viewer.
   void initViewer();
@@ -58,55 +57,62 @@ private:
   //! Clean up before .
   void cleanup();
 
-//! @name GLWF callbacks
-private:
+  //! @name GLWF callbacks
+ private:
   //! Window resize event.
-  void onResize (int theWidth, int theHeight);
+  void onResize(int theWidth, int theHeight);
 
   //! Mouse scroll event.
-  void onMouseScroll (double theOffsetX, double theOffsetY);
+  void onMouseScroll(double theOffsetX, double theOffsetY);
 
   //! Mouse click event.
-  void onMouseButton (int theButton, int theAction, int theMods);
+  void onMouseButton(int theButton, int theAction, int theMods);
 
   //! Mouse move event.
-  void onMouseMove (int thePosX, int thePosY);
+  void onMouseMove(int thePosX, int thePosY);
 
-//! @name GLWF callbacks (static functions)
-private:
-
+  //! @name GLWF callbacks (static functions)
+ private:
   //! GLFW callback redirecting messages into Message::DefaultMessenger().
-  static void errorCallback (int theError, const char* theDescription);
+  static void errorCallback(int theError, const char* theDescription);
 
   //! Wrapper for glfwGetWindowUserPointer() returning this class instance.
-  static GlfwOcctView* toView (GLFWwindow* theWin);
+  static GlfwOcctView* toView(GLFWwindow* theWin);
 
   //! Window resize callback.
-  static void onResizeCallback (GLFWwindow* theWin, int theWidth, int theHeight)
-  { toView(theWin)->onResize (theWidth, theHeight); }
+  static void onResizeCallback(GLFWwindow* theWin, int theWidth, int theHeight)
+  {
+    toView(theWin)->onResize(theWidth, theHeight);
+  }
 
   //! Frame-buffer resize callback.
-  static void onFBResizeCallback (GLFWwindow* theWin, int theWidth, int theHeight)
-  { toView(theWin)->onResize (theWidth, theHeight); }
+  static void onFBResizeCallback(GLFWwindow* theWin, int theWidth, int theHeight)
+  {
+    toView(theWin)->onResize(theWidth, theHeight);
+  }
 
   //! Mouse scroll callback.
-  static void onMouseScrollCallback (GLFWwindow* theWin, double theOffsetX, double theOffsetY)
-  { toView(theWin)->onMouseScroll (theOffsetX, theOffsetY); }
+  static void onMouseScrollCallback(GLFWwindow* theWin, double theOffsetX, double theOffsetY)
+  {
+    toView(theWin)->onMouseScroll(theOffsetX, theOffsetY);
+  }
 
   //! Mouse click callback.
-  static void onMouseButtonCallback (GLFWwindow* theWin, int theButton, int theAction, int theMods)
-  { toView(theWin)->onMouseButton (theButton, theAction, theMods); }
+  static void onMouseButtonCallback(GLFWwindow* theWin, int theButton, int theAction, int theMods)
+  {
+    toView(theWin)->onMouseButton(theButton, theAction, theMods);
+  }
 
   //! Mouse move callback.
-  static void onMouseMoveCallback (GLFWwindow* theWin, double thePosX, double thePosY)
-  { toView(theWin)->onMouseMove ((int )thePosX, (int )thePosY); }
+  static void onMouseMoveCallback(GLFWwindow* theWin, double thePosX, double thePosY)
+  {
+    toView(theWin)->onMouseMove((int)thePosX, (int)thePosY);
+  }
 
-private:
-
+ private:
   Handle(GlfwOcctWindow) myOcctWindow;
   Handle(V3d_View) myView;
   Handle(AIS_InteractiveContext) myContext;
-
 };
 
 #endif // _GlfwOcctView_Header
--- src/GlfwOcctWindow.cpp
+++ src/GlfwOcctWindow.cpp
@@ -21,16 +21,16 @@
 
 #include "GlfwOcctWindow.h"
 
-#if defined (__APPLE__)
-  #undef Handle // avoid name collisions in macOS headers
-  #define GLFW_EXPOSE_NATIVE_COCOA
-  #define GLFW_EXPOSE_NATIVE_NSGL
-#elif defined (_WIN32)
-  #define GLFW_EXPOSE_NATIVE_WIN32
-  #define GLFW_EXPOSE_NATIVE_WGL
+#if defined(__APPLE__)
+#undef Handle // avoid name collisions in macOS headers
+#define GLFW_EXPOSE_NATIVE_COCOA
+#define GLFW_EXPOSE_NATIVE_NSGL
+#elif defined(_WIN32)
+#define GLFW_EXPOSE_NATIVE_WIN32
+#define GLFW_EXPOSE_NATIVE_WGL
 #else
-  #define GLFW_EXPOSE_NATIVE_X11
-  #define GLFW_EXPOSE_NATIVE_GLX
+#define GLFW_EXPOSE_NATIVE_X11
+#define GLFW_EXPOSE_NATIVE_GLX
 #endif
 #include <GLFW/glfw3.h>
 #include <GLFW/glfw3native.h>
@@ -39,24 +39,21 @@
 // Function : GlfwOcctWindow
 // Purpose  :
 // ================================================================
-GlfwOcctWindow::GlfwOcctWindow (int theWidth, int theHeight, const TCollection_AsciiString& theTitle)
-: myGlfwWindow (glfwCreateWindow (theWidth, theHeight, theTitle.ToCString(), NULL, NULL)),
-  myXLeft  (0),
-  myYTop   (0),
-  myXRight (0),
-  myYBottom(0)
+GlfwOcctWindow::GlfwOcctWindow(int theWidth, int theHeight, const TCollection_AsciiString& theTitle) :
+    myGlfwWindow(glfwCreateWindow(theWidth, theHeight, theTitle.ToCString(), NULL, NULL)), myXLeft(0), myYTop(0),
+    myXRight(0), myYBottom(0)
 {
   if (myGlfwWindow != nullptr)
   {
     int aWidth = 0, aHeight = 0;
-    glfwGetWindowPos (myGlfwWindow, &myXLeft, &myYTop);
+    glfwGetWindowPos(myGlfwWindow, &myXLeft, &myYTop);
     glfwGetWindowSize(myGlfwWindow, &aWidth, &aHeight);
     myXRight  = myXLeft + aWidth;
     myYBottom = myYTop + aHeight;
 
-  #if !defined(_WIN32) && !defined(__APPLE__)
-    myDisplay = new Aspect_DisplayConnection ((Aspect_XDisplay* )glfwGetX11Display());
-  #endif
+#if !defined(_WIN32) && !defined(__APPLE__)
+    myDisplay = new Aspect_DisplayConnection((Aspect_XDisplay*)glfwGetX11Display());
+#endif
   }
 }
 
@@ -68,7 +65,7 @@ void GlfwOcctWindow::Close()
 {
   if (myGlfwWindow != nullptr)
   {
-    glfwDestroyWindow (myGlfwWindow);
+    glfwDestroyWindow(myGlfwWindow);
     myGlfwWindow = nullptr;
   }
 }
@@ -79,12 +76,12 @@ void GlfwOcctWindow::Close()
 // ================================================================
 Aspect_Drawable GlfwOcctWindow::NativeHandle() const
 {
-#if defined (__APPLE__)
-  return (Aspect_Drawable)glfwGetCocoaWindow (myGlfwWindow);
-#elif defined (_WIN32)
-  return (Aspect_Drawable)glfwGetWin32Window (myGlfwWindow);
+#if defined(__APPLE__)
+  return (Aspect_Drawable)glfwGetCocoaWindow(myGlfwWindow);
+#elif defined(_WIN32)
+  return (Aspect_Drawable)glfwGetWin32Window(myGlfwWindow);
 #else
-  return (Aspect_Drawable)glfwGetX11Window (myGlfwWindow);
+  return (Aspect_Drawable)glfwGetX11Window(myGlfwWindow);
 #endif
 }
 
@@ -94,12 +91,12 @@ Aspect_Drawable GlfwOcctWindow::NativeHandle() const
 // ================================================================
 Aspect_RenderingContext GlfwOcctWindow::NativeGlContext() const
 {
-#if defined (__APPLE__)
-  return (NSOpenGLContext*)glfwGetNSGLContext (myGlfwWindow);
-#elif defined (_WIN32)
-  return glfwGetWGLContext (myGlfwWindow);
+#if defined(__APPLE__)
+  return (NSOpenGLContext*)glfwGetNSGLContext(myGlfwWindow);
+#elif defined(_WIN32)
+  return glfwGetWGLContext(myGlfwWindow);
 #else
-  return glfwGetGLXContext (myGlfwWindow);
+  return glfwGetGLXContext(myGlfwWindow);
 #endif
 }
 
@@ -107,28 +104,19 @@ Aspect_RenderingContext GlfwOcctWindow::NativeGlContext() const
 // Function : IsMapped
 // Purpose  :
 // ================================================================
-Standard_Boolean GlfwOcctWindow::IsMapped() const
-{
-  return glfwGetWindowAttrib (myGlfwWindow, GLFW_VISIBLE) != 0;
-}
+Standard_Boolean GlfwOcctWindow::IsMapped() const { return glfwGetWindowAttrib(myGlfwWindow, GLFW_VISIBLE) != 0; }
 
 // ================================================================
 // Function : Map
 // Purpose  :
 // ================================================================
-void GlfwOcctWindow::Map() const
-{
-  glfwShowWindow (myGlfwWindow);
-}
+void GlfwOcctWindow::Map() const { glfwShowWindow(myGlfwWindow); }
 
 // ================================================================
 // Function : Unmap
 // Purpose  :
 // ================================================================
-void GlfwOcctWindow::Unmap() const
-{
-  glfwHideWindow (myGlfwWindow);
-}
+void GlfwOcctWindow::Unmap() const { glfwHideWindow(myGlfwWindow); }
 
 // ================================================================
 // Function : DoResize
@@ -136,10 +124,10 @@ void GlfwOcctWindow::Unmap() const
 // ================================================================
 Aspect_TypeOfResize GlfwOcctWindow::DoResize()
 {
-  if (glfwGetWindowAttrib (myGlfwWindow, GLFW_VISIBLE) == 1)
+  if (glfwGetWindowAttrib(myGlfwWindow, GLFW_VISIBLE) == 1)
   {
     int anXPos = 0, anYPos = 0, aWidth = 0, aHeight = 0;
-    glfwGetWindowPos (myGlfwWindow, &anXPos, &anYPos);
+    glfwGetWindowPos(myGlfwWindow, &anXPos, &anYPos);
     glfwGetWindowSize(myGlfwWindow, &aWidth, &aHeight);
     myXLeft   = anXPos;
     myXRight  = anXPos + aWidth;
@@ -156,6 +144,6 @@ Aspect_TypeOfResize GlfwOcctWindow::DoResize()
 Graphic3d_Vec2i GlfwOcctWindow::CursorPosition() const
 {
   Graphic3d_Vec2d aPos;
-  glfwGetCursorPos (myGlfwWindow, &aPos.x(), &aPos.y());
-  return Graphic3d_Vec2i ((int )aPos.x(), (int )aPos.y());
+  glfwGetCursorPos(myGlfwWindow, &aPos.x(), &aPos.y());
+  return Graphic3d_Vec2i((int)aPos.x(), (int)aPos.y());
 }
